42088b7cb866f9eb6580a5a073729609236fdd4d
Md Abedin <mabedin1@stuy.edu>
2016-12-21 14:51:16 -0500

    started gui

--- Other/Temperature.java
+++ Other/Temperature.java
@@ -1,4 +1,15 @@
-public class Temperature{
+import javax.swing.*;
+import java.awt.*;
+
+public class Temperature extends JFrame{
+    private Container pane;
+
+    private JButton b;
+    private JLabel C;
+    private JLabel F;
+    private JTextField c;
+    private JTextField f;
+
     public static double CtoF(double t){
 	return ((t * 1.8) + 32);
     }
@@ -7,9 +18,24 @@ public class Temperature{
 	return ((t - 32) / 1.8);
     }
 
+    this.setTitle("Farenheit/Celsius Converter");
+    this.setSize(600,400);
+    this.setLocation(100,100);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+    pane = this.getContentPane();
+    pane.setLayout(new FlowLayout());
+    
+    b = new JButton("Convert");
+    l = new JLabel("This is AWESOME! (lies)",null,JLabel.CENTER);
+    t = new JTextField(12);
+    c = new JCheckBox("Overclock!!!");
+    pane.add(l);
+    pane.add(b);
+    pane.add(t);
+    pane.add(c);
     public static void main(String[] a){
-	System.out.println(Temperature.CtoF(-40.0));
-	System.out.println(Temperature.CtoF(100.0));
-	System.out.println(Temperature.FtoC(212.0));
+	Temperature w = new Temperature();
+	w.setVisible(true);
     }
 }
